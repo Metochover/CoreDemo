4e655888a222f9a63d0f118cd4ed524e41c2f46c
Metochover <33metehan23@gmail.com>
2022-11-05 23:06:32 +0300

    CoreDemo
    Private

---
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    //bağlantı adresini tanımlayacağız
+    //contextler DbContext ten miras-kalıtım(inheritance) alır
+    public class Context:DbContext
+    {
+        //tanımladığımız method bağlantı string tanımlıyacağız
+        //
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=Metehan-AYDIN;database=CoreBlogDb; integrated security=true;  ");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ CoreDemo/EntityLayer/Concrete/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //Erişim Belirteci Değişken Türü İsim {get set}
+        [Key]
+
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; } //İlişkili tablolarda kategori silme işlemi sıkıntı olduğundan aktif ya da pasif işlemi için kullanıcağımız depişken
+
+        public List<Blog> Blogs { get; set; }
+    }
+}
